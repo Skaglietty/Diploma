e63248ceafd4dc8876c0e332e4ded50538a9a0f6
Skaglietty <skaglietty@rambler.ru>
2024-10-12 21:23:30 +0300

    Запись” git push
    git commit -m Запись
    git commit -m Запись”

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = "https://0ec55ce1-528b-4ce0-9271-f21e3b49ee23.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+
+CREATE_ORDERS = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track?t="
